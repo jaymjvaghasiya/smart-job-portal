fb8b2f348aacd3b405832b19ae203036da50e5b9
Jaykumar Vaghasiya <200120116041@git.org.in>
2026-05-31 07:00:59 +0530

    user-service commit 3

--- user-service/src/main/resources/db/migration/V3__alter_users_id_auto_increment.sql
+++ user-service/src/main/resources/db/migration/V3__alter_users_id_auto_increment.sql
@@ -1,2 +1,16 @@
+-- Drop foreign key
+ALTER TABLE user_skills
+DROP FOREIGN KEY user_skills_ibfk_1;
+
+-- Modify parent table column
 ALTER TABLE users
 MODIFY COLUMN id BIGINT NOT NULL AUTO_INCREMENT;
+
+-- Modify child table column
+ALTER TABLE user_skills
+MODIFY COLUMN user_id BIGINT NOT NULL;
+
+-- Add foreign key again
+ALTER TABLE user_skills
+ADD CONSTRAINT user_skills_ibfk_1
+FOREIGN KEY (user_id) REFERENCES users(id);
